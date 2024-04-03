c37d5e1d05ee5a96801ed9187b91556ab16d7931
Ruthikakanapuram <113005633+Ruthikakanapuram@users.noreply.github.com>
2024-04-03 13:57:04 +0530

    client.js

--- public/client.js
+++ public/client.js
@@ -1,1 +1,53 @@
+const socket = io()
 
+let username;
+let textarea = document.querySelector('#textarea')
+let messageArea = document.querySelector('.message__area')
+do{
+  username= prompt("please enter your name:")
+}while(!username);
+
+textarea.addEventListener('keyup',(e)=>{
+    if(e.key === 'Enter'){
+        sendMessage(e.target.value)
+    }
+})
+
+function sendMessage(message){
+    let msg={
+        user:username,
+        message:message.trim()
+    }
+    //append message
+    appendMessage(msg,'outgoing')
+    textarea.value=''
+    scrollToBottom()
+
+    //sent to server
+    socket.emit('message',msg)
+}
+
+function appendMessage(msg,type){
+    let mainDiv = document.createElement('div')
+    let className = type
+    mainDiv.classList.add(className,'message')
+
+    let markup = `
+    <h4>${msg.user}</h4>
+    <p>${msg.message}</p>
+    `
+    mainDiv.innerHTML=markup
+
+    messageArea.appendChild(mainDiv)
+}
+
+//receive messages
+socket.on('message',(msg)=>{
+// console.log(msg)
+appendMessage(msg,'incoming')
+scrollToBottom()
+})
+
+function scrollToBottom(){
+    messageArea.scrollTop = messageArea.scrollHeight
+}
